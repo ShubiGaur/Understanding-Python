18b02eb95dd54f65892b74576ee098d671c50530
ShubiGaur <shubigaur10@gmail.com>
2024-03-31 18:36:12 -0400

    Adding 8th module

---
+++ 8. Python-Operators/app37.py
@@ -0,0 +1,28 @@
+# Operators are used to perform operations on variables and values.
+
+# ADDITION
+print(10 + 5) # outputs 15
+
+# SUBTRACTION
+print(13 - 2) # outputs 11
+
+# MULTIPLICATION
+print(40 * 5) # outputs 200
+
+# DIVISION
+print(500 / 50) # outputs 10
+
+# MODULUS
+print(25 % 4) # outputs 1
+
+# EXPONENTIATION
+print(2 ** 4) # outputs 16
+
+# FLOOR DIVISION (rounds the result down to the nearest whole number)
+print(18 // 7) # outputs 2
+
+# Go to this site to learn about operators in full detail: 
+# https://www.w3schools.com/python/python_operators.asp
+
+
+
